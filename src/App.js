c6414ef52381af5a25838cb3de6e62f4df3bc160
Jorge Urquiza <jor.urq92@gmail.com>
2023-03-16 19:46:04 -0300

    agregando NavBar

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 
 //Componentes
 import Header from "./components/header/header"
+import NavBar from "./components/NavBar/NavBar";
 
 class App extends Component {
   render () {
     return (
     <div className="Header"> 
       <Header/>
+      <NavBar/>
     </div>
     )
   }
