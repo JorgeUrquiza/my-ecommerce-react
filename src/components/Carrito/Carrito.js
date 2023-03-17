da4827e820f23c021a9c2b1bebb98741e3715a38
Jorge Urquiza <jor.urq92@gmail.com>
2023-03-17 19:57:09 -0300

    agregando Carrito

---
+++ src/components/Carrito/Carrito.js
@@ -0,0 +1,13 @@
+import React from "react";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+
+const Carrito = () => {
+    return (
+        <div>
+            <AddShoppingCartIcon/>
+            <span>0</span>
+        </div>
+    )
+}
+
+export default Carrito;
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,13 +1,23 @@
 import React from "react";
-import "./NavBar.css"
+import "./NavBar.css";
+import Carrito from "../Carrito/Carrito";
 
 const NavBar = () => {
     return ( 
         <nav className="Navigation">
             <ul className="List-ul">
-                <li className="NavLink"><a href="https://github.com/JorgeUrquiza?tab=repositories"> Inicio </a></li>
-                <li className="NavLink"><a href="https://github.com/JorgeUrquiza?tab=repositories"> Producto </a></li>
-                <li className="NavLink"><a href="https://github.com/JorgeUrquiza?tab=repositories"> Nosotros </a></li>
+                <li className="NavLink">
+                    <a href="https://github.com/JorgeUrquiza?tab=repositories"> Inicio </a>
+                </li>
+                <li className="NavLink">
+                    <a href="https://github.com/JorgeUrquiza?tab=repositories"> Producto </a>
+                </li>
+                <li className="NavLink">
+                    <a href="https://github.com/JorgeUrquiza?tab=repositories"> Nosotros </a>
+                </li>
+                <li className="NavLink">
+                    <Carrito/>
+                </li>
             </ul>
         </nav>
     ) 
