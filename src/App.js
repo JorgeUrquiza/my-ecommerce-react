f2ee43271d1126b25cb8bc6c0c7f5859b7c7d23f
Jorge Urquiza <jor.urq92@gmail.com>
2023-03-19 13:30:42 -0300

    agrego cards

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 
 //Componentes
-import Header from "./components/header/header"
+import Header from "./components/header/header";
 import NavBar from "./components/NavBar/NavBar";
+import CardPet from "./components/Cards/CardPet";
 
 class App extends Component {
   render () {
     return (
     <div className="Header">
       <NavBar/> 
-      <Header/>
+      <Header/>      
+      <div className="Cards">
+        <CardPet name="Perros" description="Aqui encontras todo lo que necesitas para tus amigos perrunos..."
+        img="https://www.clarin.com/img/2021/07/24/kRVeytL9z_720x0__1.jpg" />
+      </div>
     </div>
     )
   }
