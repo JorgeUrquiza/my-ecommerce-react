ee735de14079739f3ef9a0768df77e158a8d62a0
Jorge Urquiza <jor.urq92@gmail.com>
2023-03-19 14:17:28 -0300

    agregando mas cards

--- src/App.js
+++ src/App.js
@@ -13,8 +13,26 @@ class App extends Component {
       <NavBar/> 
       <Header/>      
       <div className="Cards">
-        <CardPet name="Perros" description="Aqui encontras todo lo que necesitas para tus amigos perrunos..."
-        img="https://www.clarin.com/img/2021/07/24/kRVeytL9z_720x0__1.jpg" />
+        <CardPet 
+        name="Perros" 
+        description="Aqui encontras todo lo que necesitas para tus amigos perrunos..."
+        img="https://www.clarin.com/img/2021/07/24/kRVeytL9z_720x0__1.jpg"
+        />
+        <CardPet 
+        name="Gatos" 
+        description="Aqui encontras todo lo que necesitas para tus michis..."
+        img="https://www.foyel.com/archivos/2/6/pediatria_felina_web.jpg"
+        />
+        <CardPet 
+        name="Peces" 
+        description="Peceras, alimento, accesorios y decoracion. Algo mas? aqui tenes todo para que tus peces se sientan en un oceano..."
+        img="https://images.hola.com/imagenes/mascotas/20220322206704/asi-duermen-peces-curiosidades-dn/1-65-799/como-duermen-peces-ojos-abiertos-t.jpg"
+        />
+        <CardPet 
+        name="Veterinarios" 
+        description="La salud de nuestras mascotas es lo mas importante, devolveles el amor que ellos te dan con la mejor atención..."
+        img="https://www.prensalibre.com/wp-content/uploads/2019/01/Nota-cuidados-1.jpg?quality=52"
+        />
       </div>
     </div>
     )
--- src/components/Cards/CardPet.js
+++ src/components/Cards/CardPet.js
@@ -6,25 +6,24 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 
-const CardPet = (props) => {
+const CardPet = ( { img, name, description } ) => {
     return (
         <Card sx={{ maxWidth: 345 }}>
             <CardMedia
-                sx={{ height: 140 }}
-                image={props.img}
+                sx={{ height: 150 }}
+                image={img}
                 title="green iguana"
             />
             <CardContent>
                 <Typography gutterBottom variant="h5" component="div">
-                    {props.name}
+                    {name}
                 </Typography>
                 <Typography variant="body2" color="text.secondary">
-                    {props.description}
+                    {description}
                 </Typography>
             </CardContent>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                <Button size="small">Ver</Button>
             </CardActions>
         </Card>
     );
