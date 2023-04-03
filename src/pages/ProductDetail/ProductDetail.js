212564fc392340f0ba4ba5cb14806fe8da22c9e3
Jorge Urquiza <jor.urq92@gmail.com>
2023-04-03 16:27:15 -0300

    agregando detalles

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
+import { useParams } from 'react-router-dom';
+import CardProductMui from '../../components/Cards/CardProductMui';
 
 const ProductDetail = () => {
+  const [Product, setProduct] = useState({});
+
+  let {id} = useParams();
+
+
+  useEffect(() => {
+    axios(`https://my-json-server.typicode.com/JorgeUrquiza/ApiPets/Productos/${id}`).then(res =>
+    setProduct(res.data))
+}, [id]);
+
+
   return (
-    <div>ProductDetail</div>
+    <div style={{display:"flex", justifyContent:"center", flexDirection:"column", alignItems:"center"}}>
+      <h1>Productos detalles</h1>
+      <CardProductMui data={Product}/>
+    </div>
   )
 }
 
