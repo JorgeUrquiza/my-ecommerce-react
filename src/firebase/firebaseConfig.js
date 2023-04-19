efee32f9fc436c633c8ff7a51c692994f5c2bfe1
Jorge Urquiza <jor.urq92@gmail.com>
2023-04-19 09:27:09 -0300

    firebase instalado

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDpGP-0pZR9EAh4z4KCqBDe6KUlCVIKQTU",
+    authDomain: "my-ecommerce-curso-react.firebaseapp.com",
+    projectId: "my-ecommerce-curso-react",
+    storageBucket: "my-ecommerce-curso-react.appspot.com",
+    messagingSenderId: "787587122465",
+    appId: "1:787587122465:web:710d89022a7413ac8faa60",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
